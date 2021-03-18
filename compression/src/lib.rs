d52efd2b805885e35e089ae4b33177dff13b0b93
Tycho Andersen <tycho@tycho.pizza>
2021-03-18 09:02:48 -0600

    compression: add zstd-seekable support
    This implements the Compression framework with some basic zstd stupport so
    we can start compressing images.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.pizza>

--- compression/src/lib.rs
+++ compression/src/lib.rs
@@ -1,21 +1,32 @@
 use std::fs;
 use std::io;
 
-pub trait Compressor: io::Write {}
+mod zstd;
+pub use zstd::*;
+
+pub trait Compressor: io::Write {
+    fn end(&mut self) -> io::Result<()>;
+}
+
+impl Compressor for fs::File {
+    fn end(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
 
 pub trait Decompressor: io::Read + io::Seek + Send {}
 
 impl Decompressor for fs::File {}
 
 pub trait Compression {
-    fn compress(dest: fs::File) -> Box<dyn io::Write>;
+    fn compress(dest: fs::File) -> Box<dyn Compressor>;
     fn decompress(source: fs::File) -> Box<dyn Decompressor>;
 }
 
 pub struct Noop {}
 
 impl Compression for Noop {
-    fn compress(dest: fs::File) -> Box<dyn io::Write> {
+    fn compress(dest: fs::File) -> Box<dyn Compressor> {
         Box::new(dest)
     }
 
@@ -31,11 +42,11 @@ mod tests {
 
     const TRUTH: &'static str = "meshuggah rocks";
 
-    fn compress_decompress_noop<C: Compression>() {
+    pub fn compress_decompress_noop<C: Compression>() {
         let f = NamedTempFile::new().unwrap();
-        C::compress(f.reopen().unwrap())
-            .write(TRUTH.as_bytes())
-            .unwrap();
+        let mut compressed = C::compress(f.reopen().unwrap());
+        compressed.write(TRUTH.as_bytes()).unwrap();
+        compressed.end().unwrap();
 
         let mut buf = vec![0_u8; TRUTH.len()];
         C::decompress(f.reopen().unwrap()).read(&mut buf).unwrap();
@@ -43,11 +54,33 @@ mod tests {
         assert_eq!(TRUTH.as_bytes(), buf);
     }
 
-    #[test]
-    fn test_noop_compression() {
-        compress_decompress_noop::<Noop>();
+    pub fn compression_is_seekable<C: Compression>() {
+        let f = NamedTempFile::new().unwrap();
+        let mut compressed = C::compress(f.reopen().unwrap());
+        compressed.write(TRUTH.as_bytes()).unwrap();
+        compressed.end().unwrap();
 
-        // shouldn't mangle the content if in no-op mode
+        let mut buf = vec![0_u8; 5];
+        let mut decompressor = C::decompress(f.reopen().unwrap());
+        decompressor.seek(io::SeekFrom::Start("meshuggah ".len() as u64)).unwrap();
+        decompressor.read(&mut buf).unwrap();
+
+        assert_eq!("rocks".as_bytes(), &buf[0..5]);
+    }
+
+    #[test]
+    fn test_noop_roundtrip() {
+        compress_decompress_noop::<Noop>();
+    }
+
+    #[test]
+    fn test_noop_seekable() {
+        compression_is_seekable::<Noop>();
+    }
+
+    #[test]
+    fn test_noop_is_noop() {
+        // shouldn't mangle the file content if in no-op mode
         let f = NamedTempFile::new().unwrap();
         Noop::compress(f.reopen().unwrap())
             .write(TRUTH.as_bytes())
