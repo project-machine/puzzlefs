817d67b0ef6cba0893420061caf0c46651947c0e
Tycho Andersen <tycho@tycho.pizza>
2021-03-23 14:15:27 -0600

    exe: add mount command
    This also abstracts the mount code a little bit.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.pizza>

--- exe/src/main.rs
+++ exe/src/main.rs
@@ -3,9 +3,13 @@ extern crate clap;
 use std::path::Path;
 
 use clap::Clap;
+use signal_hook::consts::TERM_SIGNALS;
+use signal_hook::iterator::exfiltrator::SignalOnly;
+use signal_hook::iterator::SignalsInfo;
 
 use builder::build_initial_rootfs;
 use oci::Image;
+use reader::mount;
 
 #[derive(Clap)]
 #[clap(version = "0.1.0", author = "Tycho Andersen <tycho@tycho.pizza>")]
@@ -17,6 +21,7 @@ struct Opts {
 #[derive(Clap)]
 enum SubCommand {
     Build(Build),
+    Mount(Mount),
 }
 
 #[derive(Clap)]
@@ -24,6 +29,13 @@ struct Build {
     rootfs: String,
     oci_dir: String,
     tag: String,
+}
+
+#[derive(Clap)]
+struct Mount {
+    oci_dir: String,
+    tag: String,
+    mountpoint: String,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -35,6 +47,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             let image = Image::new(oci_dir)?;
             let desc = build_initial_rootfs(rootfs, &image)?;
             image.add_tag(b.tag, desc)
+        }
+        SubCommand::Mount(m) => {
+            // TODO: add --background option?
+            let oci_dir = Path::new(&m.oci_dir);
+            let image = Image::new(oci_dir)?;
+            let mountpoint = Path::new(&m.mountpoint);
+            let _bg = mount(&image, &m.tag, mountpoint)?;
+            let mut signals = SignalsInfo::<SignalOnly>::new(TERM_SIGNALS);
+            for s in &mut signals {
+                eprintln!("got signal {:?}, exiting puzzlefs fuse mount", s);
+            }
+            // we can return, which will ->drop() _bg and kill the thread.
+            Ok(())
         }
     }
 }
--- reader/src/lib.rs
+++ reader/src/lib.rs
@@ -1,4 +1,26 @@
-mod error;
-mod fuse;
+extern crate fuse as fuse_ffi;
+
+use std::path::Path;
+
+use oci::Image;
+
 mod puzzlefs;
+pub use puzzlefs::{Inode, InodeMode, PuzzleFS};
+
+pub mod fuse;
 pub use crate::fuse::Fuse;
+
+mod error;
+pub use error::{FSError, FSResult};
+
+pub fn mount<'a>(
+    image: &'a Image,
+    tag: &str,
+    mountpoint: &Path,
+) -> FSResult<fuse_ffi::BackgroundSession<'a>> {
+    let pfs = PuzzleFS::open(&image, tag)?;
+    let fuse = Fuse::new(pfs);
+    let session = fuse_ffi::Session::new(fuse, mountpoint, &[])?;
+    let bg = unsafe { fuse_ffi::BackgroundSession::new(session) }?;
+    Ok(bg)
+}
