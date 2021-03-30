fa234f25f409f39b3b8f688d4ee71109fa5f2a3c
Tycho Andersen <tycho@tycho.pizza>
2021-03-30 07:21:51 -0600

    reader/walk: add paths to results
    Probably we should implement something like std::fs::DirEntry for this, but
    for now let's just expose everything manually.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.pizza>

--- reader/src/walk.rs
+++ reader/src/walk.rs
@@ -1,4 +1,5 @@
 use std::collections::VecDeque;
+use std::path::PathBuf;
 
 use super::error::FSResult;
 use super::puzzlefs::{Inode, InodeMode, PuzzleFS};
@@ -8,35 +9,47 @@ use super::puzzlefs::{Inode, InodeMode, PuzzleFS};
 /// to do that.
 pub struct WalkPuzzleFS<'a> {
     pfs: &'a mut PuzzleFS<'a>,
-    q: VecDeque<u64>,
+    q: VecDeque<DirEntry>,
 }
 
 impl<'a> WalkPuzzleFS<'a> {
-    pub fn walk(pfs: &'a mut PuzzleFS<'a>) -> WalkPuzzleFS<'a> {
+    pub fn walk(pfs: &'a mut PuzzleFS<'a>) -> FSResult<WalkPuzzleFS<'a>> {
         let mut q = VecDeque::new();
-        q.push_back(1); // root inode number
-        WalkPuzzleFS { pfs, q }
+
+        let inode = pfs.find_inode(1)?; // root inode number
+        let de = DirEntry {
+            path: PathBuf::from("/"),
+            inode,
+        };
+        q.push_back(de);
+        Ok(WalkPuzzleFS { pfs, q })
     }
 
-    fn handle_next(&mut self, ino: u64) -> FSResult<Inode> {
-        let inode = self.pfs.find_inode(ino)?;
-        if let InodeMode::Dir { ref entries } = inode.mode {
-            for (_name, ino) in entries {
-                self.q.push_back(*ino);
+    fn add_dir_entries(&mut self, dir: &DirEntry) -> FSResult<()> {
+        if let InodeMode::Dir { ref entries } = dir.inode.mode {
+            for (name, ino) in entries {
+                let inode = self.pfs.find_inode(*ino)?;
+                let path = dir.path.join(name);
+                self.q.push_back(DirEntry { path, inode })
             }
         };
 
-        Ok(inode)
+        Ok(())
     }
 }
 
 impl Iterator for WalkPuzzleFS<'_> {
-    type Item = FSResult<Inode>;
+    type Item = FSResult<DirEntry>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let ino = self.q.pop_front()?;
-        Some(self.handle_next(ino))
+        let de = self.q.pop_front()?;
+        Some(self.add_dir_entries(&de).map(|_| de))
     }
+}
+
+pub struct DirEntry {
+    pub path: PathBuf,
+    pub inode: Inode,
 }
 
 #[cfg(test)]
@@ -57,14 +70,16 @@ mod tests {
         image.add_tag("test".to_string(), rootfs_desc).unwrap();
         let mut pfs = PuzzleFS::open(&image, "test").unwrap();
 
-        let mut walker = WalkPuzzleFS::walk(&mut pfs);
+        let mut walker = WalkPuzzleFS::walk(&mut pfs).unwrap();
 
         let root = walker.next().unwrap().unwrap();
-        assert_eq!(root.inode.ino, 1);
-        assert_eq!(root.dir_entries().unwrap().len(), 1);
+        assert_eq!(root.path.to_string_lossy(), "/");
+        assert_eq!(root.inode.inode.ino, 1);
+        assert_eq!(root.inode.dir_entries().unwrap().len(), 1);
 
         let jpg_file = walker.next().unwrap().unwrap();
-        assert_eq!(jpg_file.inode.ino, 2);
-        assert_eq!(jpg_file.file_len().unwrap(), 109466);
+        assert_eq!(jpg_file.path.to_string_lossy(), "/SekienAkashita.jpg");
+        assert_eq!(jpg_file.inode.inode.ino, 2);
+        assert_eq!(jpg_file.inode.file_len().unwrap(), 109466);
     }
 }
